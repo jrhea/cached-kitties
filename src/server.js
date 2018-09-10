0af86a0439576bfd94f10f54998bff7b411ff518
jonathan rhea <jonathan.rhea@gmail.com>
2018-09-10 01:40:24 -0500

    swagger endpoint now properly generating docs

--- src/server.js
+++ src/server.js
@@ -9,7 +9,23 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.use(require('./swagger'));
+//generate Swagger Docs
+var swaggerJSDoc = require('swagger-jsdoc');
+var path = require('path');
+var swaggerSpec = require('./swagger');
+
+// serve the swagger.json file
+app.get('/swagger/swagger.json', function(req, res) {
+    res.setHeader('Content-Type', 'application/json');
+    res.send(swaggerSpec);
+});
+
+// serve the swagger.html file
+app.get('/swagger', (req, res) => {
+  res.sendFile(path.join(__dirname + '/swagger/swagger.html'));
+})
+
+//app.use(require('./swagger'));
 app.use(require('./routes'));
 
 // serve api
