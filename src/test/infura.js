33dfb46d2d0d676971e1ac454220f849c116d391
jonathan rhea <jonathan.rhea@gmail.com>
2018-09-10 15:11:48 -0500

    finished a couple of basic infura tests

--- src/test/infura.js
+++ src/test/infura.js
@@ -8,13 +8,26 @@ chai.use(chaiHttp);
 describe('/GET :network/view_getTransactionsByHash', () => {
     it('Should return a valid respons and the result should be "0x50"', (done) => {
         chai.request(server)
-        .get('/api/infura/v1/mainnet/eth_getBlockTransactionCountByHash')
+        .get('/api/infura/v1/mainnet/view_getTransactionsByHash')
         .query({params:'["0xb3b20624f8f0f86eb50dd04688409e5cea4bd02d700bf6e79e9384d47d6a5a35"]'})
+        .end((err,res) => {
+            res.should.have.status(200);
+            res.body.should.be.a('array');
+            res.body.length.should.be.eql(parseInt("0x50"));
+            done();
+        });
+    });
+});
+describe('/GET /:network/:method', () => {
+    it('Should return a valid response containing the block and it should contain transactions', (done) => {
+        chai.request(server)
+        .get('/api/infura/v1/mainnet/eth_getBlockByHash')
+        .query({params:'["0xb3b20624f8f0f86eb50dd04688409e5cea4bd02d700bf6e79e9384d47d6a5a35",false]'})
         .end((err,res) => {
             res.should.have.status(200);
             res.body.should.be.a('object');
             res.body.should.have.property('result');
-            res.body.result.should.be.eql("0x50");
+            res.body.result.should.have.property('transactions');
             done();
         });
     });
