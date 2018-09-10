a07241cb90f9cebdae18c6cc1f72b4fc95176f44
jonathan rhea <jonathan.rhea@gmail.com>
2018-09-10 14:58:02 -0500

    fixed issue with mocha test not exiting

--- src/scripts/run-tests.sh
+++ src/scripts/run-tests.sh
@@ -2,5 +2,5 @@
 
 trap 'pgrep redis-server |xargs kill' EXIT;
 redis-server > /dev/null &
-PORT=8080 REDIS_URL=127.0.0.1 mocha --timeout 1000
+PORT=8080 REDIS_URL=127.0.0.1 mocha --exit --timeout 1000
 
