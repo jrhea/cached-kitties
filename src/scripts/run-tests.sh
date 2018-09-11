7b827bcd054f1c08b0bdc784a95cab96163177b3
jonathan rhea <jonathan.rhea@gmail.com>
2018-09-10 21:08:42 -0500

    making port a param to pass in

--- src/scripts/run-tests.sh
+++ src/scripts/run-tests.sh
@@ -2,5 +2,6 @@
 
 trap 'pgrep redis-server |xargs kill' EXIT;
 redis-server > /dev/null &
-PORT=8080 REDIS_URL=127.0.0.1 mocha --exit --timeout 1000
+
+PORT=$1 REDIS_URL=127.0.0.1 mocha --exit --timeout 1000
 
