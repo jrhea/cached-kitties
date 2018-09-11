7b2b1604de23350512089a0df2df6930b91e2fe1
jonathan rhea <jonathan.rhea@gmail.com>
2018-09-10 22:02:02 -0500

    updated documentation

--- src/scripts/run-tests.sh
+++ src/scripts/run-tests.sh
@@ -2,5 +2,5 @@
 
 trap 'pgrep redis-server |xargs kill' EXIT;
 redis-server > /dev/null &
-PORT=$1 REDIS_URL=127.0.0.1 mocha --exit --timeout 1000
+PORT=$1 REDIS_URL=127.0.0.1 mocha --exit --timeout 10000
 
